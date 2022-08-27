8722e57df4dc546d1feef437affb4aafc69bce69
Harby Trujillo <harby.trujillo@gmail.com>
2022-08-27 18:35:59 -0500

    Se usaron query parameter para cargar varias imagenes desde la CatApi URL cargando 3 en una plantilla simple en html

--- Ejercicio/main.js
+++ Ejercicio/main.js
@@ -1,10 +1,21 @@
 console.log("Holi H")
 
-const URL = "https://api.thecatapi.com/v1/images/search";
+const API_URL = "https://api.thecatapi.com/v1/images/search?limit=3";
+//aprender que usar los query parameters and selectors para poder modular la información de las API
 
-fetch(URL)
-    .then(res => res.json())
-    .then(data => {
-        const img = document.querySelector('img');
-        img.src = data[0].url
-    })
+
+//La siguiente función carga de manera asincrónica la api, la transforma a json y en constantes de imágenes asigna la información
+//En una nueva url lo carga en img 
+async function reload() {
+    const res = await fetch(API_URL);
+    const data = await res.json();
+    const img1 = document.getElementById('img1');
+    const img2 = document.getElementById('img2');
+    const img3 = document.getElementById('img3');
+    img1.src = data[0].url;
+    img2.src = data[1].url;
+    img3.src = data[2].url;
+    }
+
+reload();
+ 
